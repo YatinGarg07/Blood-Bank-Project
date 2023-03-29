66a296aacaab8f6d440baa178b28982b4fb95b59
tusharharyana <123148711+tusharharyana@users.noreply.github.com>
2023-03-29 21:50:29 +0530

    Update config.php

--- bbdms/includes/config.php
+++ bbdms/includes/config.php
@@ -1,6 +1,6 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
+define('DB_HOST','localhost:3307');
 define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','bbms');
@@ -11,6 +11,6 @@ $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(P
 }
 catch (PDOException $e)
 {
-exit("Error: " . $e->getMessage());
+exit("Errors: " . $e->getMessage());
 }
 ?>
